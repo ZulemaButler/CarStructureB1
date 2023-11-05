bdf0db2689403007501b1547f5c59e3da78dac23
ZulemaButler <zxbutl3r@gmail.com>
2023-11-05 01:43:37 -0400

    I have added all required features

--- src/test/java/main.java
+++ src/test/java/main.java
@@ -5,5 +5,13 @@ public class main {
         audi.Hp="5000";
         audi.Doors="2";
         audi.Model="A90";
+
+        CarFeature1 tire =new CarFeature1();
+        tire.tyre="4;";
+        tire.tyreColor="Pink";
+        tire.tyreModel="Yokohama";
+        tire.tyreWidth="18";
+        tire.tyreYear="2023";
+
     }
 }
